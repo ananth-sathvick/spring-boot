7585da199852acfc058d859f0d18e001f96b9b41
06swax <06swastik@gmail.com>
2021-03-04 21:05:08 +0530

    config changes

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({ "com.example.controller" })
+@ComponentScan({ "com.example" })
 @EntityScan("com.example.model")
 @EnableJpaRepositories("com.example.repository")
 public class RestServiceApplication {
