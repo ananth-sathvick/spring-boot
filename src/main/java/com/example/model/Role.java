334c7255f8808e907cf08cfc855b50318281bc3d
Ananth sathvick <ananth.sathvick@accolitedigital.com>
2021-03-05 13:11:26 +0530

    fixed role model

--- src/main/java/com/example/model/Role.java
+++ src/main/java/com/example/model/Role.java
@@ -1,9 +1,11 @@
 package com.example.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Role {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
