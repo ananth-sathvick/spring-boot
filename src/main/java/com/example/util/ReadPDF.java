61b90e84b98a7df03cc558a0096a10af68a47285
ananth-sathvick <ananth.sathvick@accolitedigital.com>
2021-03-10 17:29:20 +0530

    Added pdf parsing option

---
+++ src/main/java/com/example/util/ReadPDF.java
@@ -0,0 +1,40 @@
+package com.example.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.example.model.Expense;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+public class ReadPDF {
+
+    public String readPDF(byte[] file) throws IOException {
+
+        try (PDDocument document = PDDocument.load(file)) {
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+
+                return pdfFileInText;
+            }
+        }
+         return null;
+
+    }
+
+}
